098a2b3bf1fb129c8dbee9435af0b412c4dc10b5
Samuel Loup <samuel.loup@gmail.com>
2014-12-17 15:45:36 +0100

    new interface

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var EventStore = require ('./lib/eventstore.js');
+var moduleName    = 'eventstore';
+var xLog          = require ('xcraft-core-log') (moduleName);
+var EventStore    = require ('./lib/eventstore.js');
 var es;
 
 exports.getInstance = function () {
@@ -11,6 +13,30 @@ exports.getInstance = function () {
   var config = require ('xcraft-core-etc').load ('xcraft-core-eventstore');
   es = new EventStore (config);
   return es;
+};
+
+exports.persist= function (topic, msg) {
+  if (es) {
+    /* Note about EventStore and 'connected' topic */
+    /* we discard connected message for two reason: */
+    /* 1. eventstore don't support field name containing '.' */
+    /* 2. this topic annonce all commands, and has no business value */
+    if (msg && topic !== 'connected') {
+      es.insert (msg.token, topic, msg.data, function (err) {
+        if (err) {
+          xLog.err (err);
+        }
+      });
+    } else {
+      es.insert (msg.token, topic, null , function (err) {
+        if (err) {
+          xLog.err (err);
+        }
+      });
+    }
+  } else {
+    xLog.err ('not ready to persist');
+  }
 };
 
 /**
