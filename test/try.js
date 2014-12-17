859790049fcbf249de4e859c4a5487ab5585f0c6
Samuel Loup <samuel.loup@gmail.com>
2014-12-17 15:56:09 +0100

    updated playground

--- test/try.js
+++ test/try.js
@@ -1,25 +1,19 @@
 'use strict';
 
-var es = require ('xcraft-core-eventstore').getInstance();
-var testEvent = {
-  userId: 0,
-  activityId: 99,
-  data: {
+var es = require ('xcraft-core-eventstore');
+var eventstore = es.getInstance();
+
+/* Work in progress playground*/
+eventstore.use (function () {
+
+  var testEvent = {
     payload: 'test.test.test',
     someData: ['a', 'b', 'c']
-  }
-};
+  };
 
-es.use (function () {
-  es.findAll (0, function (err, docs) {
+  eventstore.findAll (function (err, docs) {
     console.log (err);
     console.log ('documents:' + docs.length);
-    es.insert ('test.topic', testEvent, function (err) {
-      console.log (err);
-      es.findAll (0, function (err, docs) {
-        console.log (err);
-        console.log ('documents:' + docs.length);
-      });
-    });
+    es.persist ('test.topic', testEvent);
   });
 });
